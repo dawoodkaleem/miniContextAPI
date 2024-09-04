3c021ba3ead329e05f3349eb5ee0929fcc629a46
dawoodkaleem <dawood9743@gmail.com>
2024-09-04 17:35:54 +0500

    First Comment new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  
+
+  return (
+    <>
+     
+     <h1 className="text-3xl text-center m-96 font-bold underline">
+      Hello world! Wherer
+    </h1>
+    </>
+  )
+}
+
+export default App
